7ed2988ed46e18cda74791ec8083a4da7a8ab94d
alfedd@crimson.ua.edu Fedd <alfedd@crimson.ua.edu>
2018-03-12 22:56:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-var animal = 'dog'
+var animal = 'dog'; 'cat'
 
 function myAnimal() {
   return animal
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
-  return animal
+   return animal()
+   return 'cat'
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
-  const two = 2
+  return n + two;
+  const two = 2;
 }
 
 var funkyFunction = function() {
