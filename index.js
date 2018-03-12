84098b2946cf7a3d314158a4fe292d3df3f05bef
alfedd@crimson.ua.edu Fedd <alfedd@crimson.ua.edu>
2018-03-12 22:58:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-   return animal()
+   return animal
    return 
 }
 
@@ -16,10 +16,10 @@ function add2(n) {
 
 var funkyFunction = function() {
   return function() {
-    return "FUNKY!"
+    return 'FUNKY!'
   }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var FUNKY = funkyFunction
